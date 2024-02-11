1119053a95316dfbd1cdeeacd87707c0d20d0f42
José Antonio Alacid Pérez <127385548+jaapjrc@users.noreply.github.com>
2024-02-11 13:01:25 +0100

    ErColgao

---
+++ ErColgao/app/src/main/java/com/example/ercolgao/GameConstants.kt
@@ -0,0 +1,27 @@
+package com.example.ercolgao
+
+object GameConstants {
+    val words = listOf(
+        "Hola",
+        "Adios",
+        "Circulo",
+        "Cuadrado",
+        "Triangulo",
+        "Piramide",
+        "Cilindro",
+        "Ordenador",
+        "Feliz",
+        "Amigo",
+        "Perro",
+        "Escuela",
+        "Rapido",
+        "Lento",
+        "Playa",
+        "Libro",
+        "Pelicula",
+        "Serie",
+        "Parque",
+        "Enemigo",
+        "Gato"
+    )
+}
---
+++ ErColgao/app/src/main/java/com/example/ercolgao/GameManager.kt
@@ -0,0 +1,90 @@
+package com.example.ercolgao
+
+import kotlin.random.Random
+
+class GameManager {
+
+    private var lettersUsed: String = ""
+    private lateinit var underscoreWord: String
+    private lateinit var wordToGuess: String
+    private val maxTries = 7
+    private var currentTries = 0
+    private var drawable: Int = R.drawable.img0
+
+    fun startNewGame(): GameState {
+        lettersUsed = ""
+        currentTries = 0
+        drawable = R.drawable.img7
+        val randomIndex = Random.nextInt(0, GameConstants.words.size)
+        wordToGuess = GameConstants.words[randomIndex]
+        generateUnderscores(wordToGuess)
+        return getGameState()
+    }
+
+    fun generateUnderscores(word: String) {
+        val sb = StringBuilder()
+        word.forEach { char ->
+            if (char == '/') {
+                sb.append('/')
+            } else {
+                sb.append("_")
+            }
+        }
+        underscoreWord = sb.toString()
+    }
+
+    fun play(letter: Char): GameState {
+        if (lettersUsed.contains(letter)) {
+            return GameState.Running(lettersUsed, underscoreWord, drawable)
+        }
+
+        lettersUsed += letter
+        val indexes = mutableListOf<Int>()
+
+        wordToGuess.forEachIndexed { index, char ->
+            if (char.equals(letter, true)) {
+                indexes.add(index)
+            }
+        }
+
+        var finalUnderscoreWord = "" + underscoreWord // _ _ _ _ _ _ _ -> E _ _ _ _ _ _
+        indexes.forEach { index ->
+            val sb = StringBuilder(finalUnderscoreWord).also { it.setCharAt(index, letter) }
+            finalUnderscoreWord = sb.toString()
+        }
+
+        if (indexes.isEmpty()) {
+            currentTries++
+        }
+
+        underscoreWord = finalUnderscoreWord
+        return getGameState()
+    }
+
+    private fun getHangmanDrawable(): Int {
+        return when (currentTries) {
+            0 -> R.drawable.img0
+            1 -> R.drawable.img1
+            2 -> R.drawable.img2
+            3 -> R.drawable.img3
+            4 -> R.drawable.img4
+            5 -> R.drawable.img5
+            6 -> R.drawable.img6
+            7 -> R.drawable.img7
+            else -> R.drawable.img7
+        }
+    }
+
+    private fun getGameState(): GameState {
+        if (underscoreWord.equals(wordToGuess, true)) {
+            return GameState.Won(wordToGuess)
+        }
+
+        if (currentTries == maxTries) {
+            return GameState.Lost(wordToGuess)
+        }
+
+        drawable = getHangmanDrawable()
+        return GameState.Running(lettersUsed, underscoreWord, drawable)
+    }
+}
